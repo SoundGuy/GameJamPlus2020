6544b062cf55e3ffa59781207fcd3085afa81237
Binoman <thebinoman@gmail.com>
2020-10-09 23:49:51 +0300

    Health System  - Not Complete

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    private float _hp = 10;
+    public float hp
+    {
+        get { return _hp; }
+    }
+    public float maxHealth = 100;
+
+    public UnityEvent OnDeath;
+
+    public void Hit(float amount)
+    {
+        _hp -= amount;
+        if (_hp <= 0)
+            OnDeath.Invoke();
+    }
+
+    public void Heal(float amount)
+    {
+        _hp = Mathf.Min(_hp + amount, maxHealth);
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(Animator))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void UpdateHealth(float hp, float maxHp)
+    {
+        float normalizedHealth = (maxHp != 0) ? hp / maxHp : 0;
+        animator.SetFloat("Health", normalizedHealth);
+        text.text = $"{hp} / {maxHp}";
+    }
+}
