f8661ec559d3772f13a6808208c6f40842a79c67
Oded Sharon <github@odedsharon.com>
2020-10-10 00:47:17 +0300

    Beat Manager Working.

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class BeatManager : MonoBehaviour
 {
+    [SerializeField] private int BMP;
+    
+    [SerializeField] private UnityEvent BeatAction;
+
+    
+    [SerializeField] public static float BeatLength;
+    [SerializeField] private float FirstBeat;
+    [SerializeField] private float LastBeat;
+    [SerializeField] private int playedBeat;
+    [SerializeField]  bool beatEnabled = false;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        BeatLength = 60f / BMP;
+     }
 
-    // Update is called once per frame
-    void Update()
+    public  void StartBeat()
     {
+        beatEnabled = true;
+        FirstBeat = Time.time;
+        playedBeat = 0;
+        BeatLength = 60f / BMP;        
+        GetComponent<PlayableDirector>().Play();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!beatEnabled)
+        {
+            return;
+            
+        }
         
+        
+        if ((Time.time - FirstBeat ) / (BeatLength) > playedBeat)
+        {
+            playedBeat++;
+            LastBeat = Time.time;
+            BeatAction.Invoke();
+        }
     }
 }
--- Assets/Scripts/BeatSignalReciever.cs
+++ Assets/Scripts/BeatSignalReciever.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class BeatSignalReciever : MonoBehaviour
 {
+
+    [SerializeField] float defIncreasePrec = 2f;
+    [SerializeField] float defPrecOfBeat = 0.2f;
+    
+    [SerializeField] float safetyBuffer = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,25 @@ public class BeatSignalReciever : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void IncreaseSize(float IncreasePrec)
+    {
+        if (IncreasePrec == 0f)
+        {
+            IncreasePrec = defIncreasePrec;
+        }
+
+        float PrecOfBeat = defPrecOfBeat;
+        
+        
+        Vector3 origScale = gameObject.transform.localScale;
+        Vector3 toScale = origScale * IncreasePrec;
+        float UpLength = BeatManager.BeatLength * PrecOfBeat * (1f- safetyBuffer);
+        float DownLength = BeatManager.BeatLength * (1f -PrecOfBeat) * (1f - safetyBuffer);
+        LeanTween.scale(gameObject, toScale, UpLength).setOnComplete(() =>
+            {
+                LeanTween.scale(gameObject, origScale, DownLength);
+            });
     }
 }
