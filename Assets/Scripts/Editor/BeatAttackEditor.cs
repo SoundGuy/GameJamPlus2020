a8ec3f44260f72f153962ed1ecb5adb17990411f
Oded Sharon <github@odedsharon.com>
2020-10-12 04:27:45 +0300

    Messing about with Unity UI

--- Assets/Scripts/Editor/BeatAttackEditor.cs
+++ Assets/Scripts/Editor/BeatAttackEditor.cs
@@ -11,7 +11,9 @@ public class BeatAttackEditor : EditorWindow
     private BeatAttack _beatAttack;
     private VisualElement _rootElement;
     private VisualTreeAsset _visualTree;
+    private VisualTreeAsset _visualTreeBeat;
     private VisualElement ContainerVlement;
+    private VisualElement BeatsContainer;
     private StyleSheet uss;
     
     private void OnEnable()
@@ -22,6 +24,7 @@ public class BeatAttackEditor : EditorWindow
         //_beatAttack = target as BeatAttack;
         //_rootElement = new VisualElement();
         _visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/BeatAttack.uxml");
+        _visualTreeBeat = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/BeatUiElement.uxml");
         _visualTree.CloneTree(_rootElement);
         
         
@@ -29,7 +32,7 @@ public class BeatAttackEditor : EditorWindow
         _rootElement.styleSheets.Add(uss);
         
         
-        //Debug.Log(typeof(BeatAttack).AssemblyQualifiedName);
+        
         
         var objectFields = _rootElement.Query<ObjectField>();
         objectFields.ForEach(SetupObject);
@@ -37,14 +40,16 @@ public class BeatAttackEditor : EditorWindow
         
         ContainerVlement = new VisualElement();
         _rootElement.Add(ContainerVlement);
-        
+
+        BeatsContainer = _rootElement.Q<VisualElement>("BeatsContainer");
+
     }
 
     private void SetupObject(ObjectField obj)
     {
         if (obj.name == "BeatAttackOBJ")
         {
-            Debug.Log("Success " + obj.value);
+            //Debug.Log("Success " + obj.value);
             obj.RegisterValueChangedCallback(ObjChangedCallback);
         }
     }
@@ -54,25 +59,78 @@ public class BeatAttackEditor : EditorWindow
 
         Debug.Log("ValueChanged " + evt.newValue);
         
-        _beatAttack = evt.newValue as BeatAttack;
+        _beatAttack = evt.newValue as BeatAttack;        
         
         ContainerVlement.Clear();
+        BeatsContainer.Clear();
         
         int i = 0;
         foreach (BeatAttack.BeatDamageProperties damagePropertiese in _beatAttack.Damages)
         {
 
-            string beat = i++.ToString();
-            VisualElement element = new VisualElement();
-            element.Add(new  Label("Beat"+beat));
+            VisualElement beatEelement = CreateBeatElement(damagePropertiese,i);
+            ProgressBar progress = beatEelement.Q<ProgressBar>();
+            float progressVal = 100f * i / _beatAttack.Damages.Length ;
+
+            progress.value = progressVal; 
             
+            
+
+            
+            
+            
+            // TODO: Reogranize this
+            VisualElement element = new VisualElement();            
+            element.Add(beatEelement);            
             ContainerVlement.Add(element);
+            BeatsContainer.Add(element);
+
+
+            i++;
         }
         
         
         
     }
 
+
+    VisualElement CreateBeatElement(BeatAttack.BeatDamageProperties damagePropertiese, int beatNum)
+    {
+        VisualElement element = new VisualElement();
+        _visualTreeBeat.CloneTree(element);
+        /*
+        element.Add(new Label(damagePropertiese._damageType.ToString()));                
+        element.Add(new Label("Damage Type"));
+        
+
+        element.Add(new Label(damagePropertiese.Strentgh.ToString()));                
+        element.Add(new Label("Strentgh"));
+        */
+        
+        string beat = beatNum.ToString();
+        element.Q<Label>("BeatNum").text = "Beat " + beat;
+
+        EnumField damageType = element.Q<EnumField>("DamageType");        
+        damageType.value = damagePropertiese._damageType;
+        
+        FloatField stength = element.Q<FloatField>("strength");        
+        //stength.value = damagePropertiese.Strentgh;
+
+        SerializedObject so = new  SerializedObject(_beatAttack);
+        Debug.Log("So" + so);
+        SerializedObject so1 = new  SerializedObject(_beatAttack.Damages);
+        SerializedProperty sp =so.FindProperty("Damages[0].Strentgh");
+        Debug.Log("Sp" + sp);
+        stength.BindProperty(sp);
+
+        //Debug.Log(typeof(Sprite).AssemblyQualifiedName);
+        ObjectField spriteField= element.Q<ObjectField>("Sprite");        
+        spriteField.value = damagePropertiese.sprite;
+        
+        
+        return element;
+        
+    }
 
     [MenuItem("BeatAttack/Open _%#T")]
     public static void ShowWindow()
